760ea378d8f40c96c05ce304457364a20114cee0
yohann <yohann.durel@gmail.com>
2021-05-05 23:06:00 +0200

    etape 3-4-5

--- src/Entity/Pizza.php
+++ src/Entity/Pizza.php
@@ -16,7 +16,7 @@ class Pizza
 {
     /**
      * @var int
-     * @ORM\Column(name="id", type="integer")
+     * @ORM\Column(name="id_pizza", type="integer")
      * @ORM\Id
      * @ORM\GeneratedValue(strategy="AUTO")
      */
